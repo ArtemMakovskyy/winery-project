e51162a6ee4639f78581c0c2387369c1ef118ec1
Artem Makovskyy <photomak79@gmail.com>
2026-02-09 11:08:50 +0200

    perf(order-service): move pagination from database to memory level
    - Remove EntityGraph from paged queries in OrderRepository to fix HHH90003004.
    - Enable batch fetching to prevent N+1 issues.

--- stoic-winery-api/order-service/src/main/java/com/winestoreapp/order/repository/OrderRepository.java
+++ stoic-winery-api/order-service/src/main/java/com/winestoreapp/order/repository/OrderRepository.java
@@ -6,27 +6,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    @Modifying
-    @Query("""
-            UPDATE Order o 
-            SET o.completedTime = CURRENT_TIMESTAMP, o.paymentStatus = 'PAID'
-            WHERE o.id = :orderId""")
-    void updateOrderPaymentStatusAsPaidAndSetCurrentDate(
-            @Param("orderId") Long orderId
-    );
-
-    @EntityGraph(attributePaths = {"shoppingCard.purchaseObjects"})
     Page<Order> findAllByUserId(Long userId, Pageable pageable);
 
     @EntityGraph(attributePaths = {"shoppingCard.purchaseObjects"})
     Optional<Order> findById(Long id);
 
-    @EntityGraph(attributePaths = {"shoppingCard.purchaseObjects"})
     Page<Order> findAll(Pageable pageable);
 
     @EntityGraph(attributePaths = {"shoppingCard"})
